b12be8b01ff534cf785134bf79ea474442637da9
haghir <47360153+haghir@users.noreply.github.com>
2023-06-03 15:40:25 +0000

    Remove a submodule.

--- mysql/min-auth.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE minauth CHARSET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER minauth IDENTIFIED BY 'minauth';
-GRANT ALL ON minauth.* TO minauth;
--- mysql/wordpress.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE wordpress CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER wordpress IDENTIFIED BY 'wordpress';
-GRANT ALL ON wordpress.* TO wordpress;
