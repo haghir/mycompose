3c18607eb8a4d4351aae24e9e26961742b825ee8
haghir <47360153+haghir@users.noreply.github.com>
2023-06-11 21:02:38 +0900

    Arrange the tree.

--- prepare-mysql/prepare-minauth.sh
+++ prepare-mysql/prepare-minauth.sh
@@ -8,9 +8,9 @@ CREATE USER ${USERNAME} IDENTIFIED BY '${PASSWORD}';
 CREATE DATABASE minauth CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL ON minauth.* TO ${USERNAME};
 USE minauth;
-CREATE TABLE `credentials` (
-    `id`     VARCHAR(191) NOT NULL PRIMARY KEY
-,   `salt`   VARCHAR(191) NOT NULL
-,   `pwhash` CHAR(64) NOT NULL
+CREATE TABLE credentials (
+    id     VARCHAR(191) NOT NULL PRIMARY KEY
+,   salt   VARCHAR(191) NOT NULL
+,   pwhash CHAR(64) NOT NULL
 );
 EOF
